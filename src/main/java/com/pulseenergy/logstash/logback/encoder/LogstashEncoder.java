c9091900796008da4296887ab90eb249febc796d
Neil Gentleman <ngentleman@gmail.com>
2015-03-25 13:45:52 -0700

    move to com.pulseenergy

--- src/main/java/com/pulseenergy/logstash/logback/encoder/LogstashEncoder.java
+++ src/main/java/com/pulseenergy/logstash/logback/encoder/LogstashEncoder.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.logstash.logback.encoder;
+package com.pulseenergy.logstash.logback.encoder;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
--- src/test/java/com/pulseenergy/logstash/logback/encoder/LogstashEncoderTest.java
+++ src/test/java/com/pulseenergy/logstash/logback/encoder/LogstashEncoderTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.logstash.logback.encoder;
+package com.pulseenergy.logstash.logback.encoder;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
